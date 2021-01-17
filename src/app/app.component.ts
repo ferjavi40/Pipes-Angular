ead63b437416c6acfeccef84dd9bdcb7ab13b558
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-01-17 15:19:46 +0100

    antes del pipe personalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,35 @@ export class AppComponent {
 
   nombre: string = 'Wayne Rooney';
 
+  nombre2: string = 'fERnaNdo rAmiREZ';
+
   arreglo = [1,2,3,4,5,6,7,8,9,10];
 
   PI: number = Math.PI;
 
   porcentaje: number = 0.234;
+
+  salario: number = 1234;
+
+  fecha: Date= new Date ();
+
+
+  valorPromesa= new Promise<string>((resolve,reject)=>{
+
+    setTimeout(()=>{
+      resolve('LLego la data');
+    },3500)
+  })
+
+
+  heroe={
+    nombre: 'Fernando',
+    clave: 'Front end developer',
+    edad: 30,
+    direccion: {
+      calle: 'bajondillo',
+      ciudad: 'torremolinos'
+    }
+  }
 
 }
